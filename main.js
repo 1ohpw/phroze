9a7fea13b3c52cf0c5e8c9d944442ecaa91b59e0
Denzale Reese <denzale@denzale-macbookpro.roam.corp.google.com>
2019-01-27 16:47:51 -0800

    initial commit

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-$(function () {
-  var images
-  images = data
+$(function() {
   $.ajax({
     type: 'GET',
     url: "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2&count=5",
-    success: function(images) {
-    $.each(images, function(i, image) {
-      $images.append('<Li>name: '=IMAGE.url</Li>');
-    });
-  }
+    success: function(response) {
+      console.log(response.data._embedded.media)
+      let mediaJson = response.data._embedded.media;
+      $.each(mediaJson, function(i, image) {
+        console.log(mediaJson[i].images.normal);
+        let imgUrl = mediaJson[i].images.normal;
+        $("#gallery-img-" + i).attr("src", imgUrl);
+      });
+    }
   });
-})
+});
